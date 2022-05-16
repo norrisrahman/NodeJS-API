6f5835530705522b5ce5f72cf3e32ee9f5b1ae73
norrisrahman <norrisrahman.205009@mhs.its.ac.id>
2022-05-17 01:21:51 +0700

    Main

--- config/database.config.js
+++ config/database.config.js
@@ -1,6 +1,6 @@
 const urlDb = process.env.MONGODB_URI
 
 module.exports = {
-    // url: 'mongodb://admin:admin@cluster0-shard-00-02.gezdg.mongodb.net:27017,admin:admin@cluster0-shard-00-02.gezdg.mongodb.net:27017,admin:admin@cluster0-shard-00-02.gezdg.mongodb.net:27017/myDB?ssl=true&replicaSet=atlas-5x4u5c-shard-0&authSource=admin&retryWrites=true&w=majority'
-    url: urlDb
+    url: 'mongodb://admin:admin@cluster0-shard-00-02.gezdg.mongodb.net:27017,admin:admin@cluster0-shard-00-02.gezdg.mongodb.net:27017,admin:admin@cluster0-shard-00-02.gezdg.mongodb.net:27017/myDB?ssl=true&replicaSet=atlas-5x4u5c-shard-0&authSource=admin&retryWrites=true&w=majority'
+    // url: urlDb
 }
